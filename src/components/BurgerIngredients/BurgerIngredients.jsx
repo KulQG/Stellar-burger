bd404431e4ef9b02b306af250acc6c08f0622219
Egor Popikov <cool.egor-kot@yandex.ru>
2022-12-16 17:05:42 +0300

    feat/headersWithCards

--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components'
 import Card from '../Card/Card'
 import BIngrStyles from './BurgerIngredients.module.css'
@@ -8,6 +8,44 @@ import ingrsData from '../../utils/data.js'
 export default function BurgerIngredients(props) {
   const [current, setCurrent] = React.useState('one')
   const [cards, setCards] = React.useState(ingrsData)
+
+  //разделение ингредиентов
+  const headersWithCards = () => {
+    //создание массивов с каждым типом продукта
+    let buns = []
+    let main = []
+    let sauces = []
+
+    cards.forEach((card) => {
+      if (card.type === 'bun') {
+        buns.push(card)
+      } else if (card.type === 'main') {
+        main.push(card)
+      } else if (card.type === 'sauce') {
+        sauces.push(card)
+      }
+    })
+
+    //возврат разметки карточки 
+    //для каждого уникального массива
+    const mapMethod = (arr) => {
+      console.log(arr)
+      return arr.map((card) => {
+        return (<Card post={card} key={card._id} />)
+      })
+    }
+
+    return (
+      <>
+        <h2 className="text text_type_main-medium" >Булки</h2>
+        <div className={BIngrStyles.cards}>{mapMethod(buns)}</div>
+        <h2 className="text text_type_main-medium" >Соусы</h2>
+        <div className={BIngrStyles.cards}>{mapMethod(sauces)}</div>
+        <h2 className="text text_type_main-medium" >Начинки</h2>
+        <div className={BIngrStyles.cards}>{mapMethod(main)}</div>
+      </>
+    )
+  }
 
   const tab = () => {
     return (
@@ -30,11 +68,7 @@ export default function BurgerIngredients(props) {
       <h1 className="text text_type_main-large">Соберите Бургер</h1>
       {tab()}
       <div className={BIngrStyles.products}>
-        <div className={BIngrStyles.cards}>
-        {cards.map(cards => 
-            <Card post={cards} />
-          ) }
-        </div>
+        <div className={BIngrStyles.cards}>{headersWithCards()}</div>
       </div>
     </div>
   )
