1c120ca9f419e6b442d0f723f8b47728d7a50c76
Egor Popikov <cool.egor-kot@yandex.ru>
2022-12-17 18:46:40 +0300

    fetching

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,31 @@ import React from 'react'
 import appStyles from './App.module.css'
 import Header from '../Header/Header'
 import TotalConstructor from '../TotalConstructor/TotalConstructor'
-import ingrsData from '../../utils/data.js'
+//import {address} from '../../utils/consts.js'
 
 function App() {
+
+  const [state, setState] = React.useState({
+    isLoading: true,
+    cardData: []
+  })
+
+  React.useEffect(() => {
+    const getData = async () => {
+      setState({...state, loading: true});
+      const res = await fetch('https://norma.nomoreparties.space/api/ingredients');
+      const data = await res.json();
+      setState({ cardData: data.data, loading: false });
+    };
+    getData()
+  },[])
+
+  console.log(state.cardData)
+
   return (
     <>
       <Header />
-      <TotalConstructor arr={ingrsData} />
+      <TotalConstructor arr={state.cardData} />
     </>
   )
 }
